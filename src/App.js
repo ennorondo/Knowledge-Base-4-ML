9114e5df8c61d097a6dd0ce2f405d25ba8e6a897
Enno Ludwig <enno.ludwig@rwth-aachen.de>
2023-08-31 10:41:57 +0200

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import './components/Navbar.css'
+import './components/MLDP.css'
+import './components/ProcessViews.css'
+import Navbar from "./components/Navbar.js"
+import DevelopmentProcess from './components/MLDP.js';
+import AllProcessViews from './components/ProcessViews.js';
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar />
+        <DevelopmentProcess />
+        <AllProcessViews />
     </div>
   );
 }
-
-export default App;
