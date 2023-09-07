1e98092b211cc69b320388dc8e360fe0a892fda7
Enno Ludwig <enno.ludwig@rwth-aachen.de>
2023-09-07 13:01:18 +0200

    trying to add pages, this is a backup

---
+++ src/components/pages/Glossary.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function GlossaryPage() {
+  return (
+    <div>
+      <h1>Glossary Page</h1>
+      {/* Add your content for the About page */}
+    </div>
+  );
+}
---
+++ src/components/pages/Tools.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ToolPage() {
+  return (
+    <div>
+      <h1>Tool Page</h1>
+      {/* Add your content for the Home page */}
+    </div>
+  );
+}
